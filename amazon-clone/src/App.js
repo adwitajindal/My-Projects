f43df2d9958eacd4ce5d149687f94e8ce126746c
Adwita Jindal <adwita.jindal.42@gmail.com>
2025-03-14 00:46:43 +0530

    banner image added

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Header from './Header';
-
+import "./App.css";
+import Header from "./Header";
+import React from "react";
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
-     {/* Header Component*/}
-     <Header/>
-     {/* Home Component */}
-     <Home/>
+      {/* Header Component*/}
+      <Header />
+      {/* Home Component */}
+      <Home />
     </div>
   );
 }
---
+++ amazon-clone/src/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Home.css";
+import bannerImage from "./assets/image.png";
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <a href='https://www.amazon.in/b/ref=ohlr_bstof_Budgetstore/?_encoding=UTF8&node=21557580031&pd_rd_w=QLMTe&content-id=amzn1.sym.7a433ea4-e405-4cda-9001-e9715465e231&pf_rd_p=7a433ea4-e405-4cda-9001-e9715465e231&pf_rd_r=C1BEF9ZH4DX4P6RXXD6N&pd_rd_wg=r6PYz&pd_rd_r=49152718-7e5b-465f-8ca4-9323ac529927&ref_=pd_hp_d_hero_unk'><img className="banner-img" src={bannerImage} alt='banner'></img></a>
+      <div className='home__row'>
+        {/* Product*/}
+        {/* Product*/}
+      </div>
+      <div className='home__row'>
+        {/* Product*/}
+        {/* Product*/}
+        {/* Product*/}
+      </div>
+      <div className='home__row'>
+        {/* Product*/}
+        {/* Product*/}
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
